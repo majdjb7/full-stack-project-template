9ea26f95c0664b3f4708729ec4b9e162f1a9861b
majdjb7 <majdjd171@gmail.com>
2021-12-14 20:45:21 +0200

    initialized admin pages and models

---
+++ dist/admin/adminModel.js
@@ -0,0 +1,24 @@
+class AdminModel {
+    constructor() {
+        this.data = {}
+
+    }
+
+    getUserProccess() {
+        $.get(`/studentPage/allprocesses/osama`, function (res) {
+            this.data = res;
+        })
+    }
+
+    addUserProccess(newProccess) {
+        console.log(newProccess);
+        $.ajax({
+            url: `/studentPage/process/`,
+            method: "POST",
+            data: newProccess
+        })
+    }
+
+    // addProccess()
+}
+
