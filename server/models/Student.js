daa2fffabfda22d833d6409800a152fbe19288f7
Shokre <shokreserhan123@gmail.com>
2021-12-14 11:56:08 +0200

    change cohort type

--- server/models/Student.js
+++ server/models/Student.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema
 const studentSchema = new Schema({
     name: String,
     email: String,
+    password: String,
     phone: String,
-    Cohort: String,
+    Cohort: Number,
     Processes: [{ type: Schema.Types.ObjectId, ref: 'Process' }]
 })
 
