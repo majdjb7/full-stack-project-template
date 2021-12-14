5f68ad7188924c20bd61bbdfa6b1ae26407420fc
SARA ZIADA <sziada276@gmail.com>
2021-12-14 23:15:33 +0200

    post interview to process for student

--- server/routes/studentApi.js
+++ server/routes/studentApi.js
@@ -7,26 +7,32 @@ const Interview = require('../models/Interview')
 router.post('/process/:studentName', async function(req, res) {
     let process = req.body
     let student = await Student.find({ name: req.params.studentName })
-    console.log(student)
     let counter = student[0].ProcessesCounter + 1
     let newProcess = new Process({
         Id: counter,
         JobTitle: process.JobTitle,
         companyName: process.companyName,
-        link: process.link
+        link: process.link,
+        StudentId: student[0]._id
     })
     newProcess.save()
 
-    student = await Student.findOneAndUpdate({ name: req.params.studentName }, {
-        $push: { Processes: newProcess },
-        $set: { ProcessesCounter: counter }
-    }, { new: true });
-    res.send(student)
+    await Student.findOneAndUpdate({ name: req.params.studentName }, {
+            $push: { Processes: newProcess },
+            $set: { ProcessesCounter: counter }
+        }, { new: true })
+        .populate("Processes")
+        .exec(function(err, student) {
+            res.send(student)
+        });
+
 })
 
 router.get('/allprocesses/:studentName', async function(req, res) {
-    const student = await Student.find({ name: req.params.studentName })
-    res.send(student)
+    const student = await Student.find({ name: req.params.studentName }).populate("Processes")
+        .exec(function(err, student) {
+            res.send(student)
+        });
 })
 
 router.get('/allprocesses', async function(req, res) {
@@ -34,17 +40,23 @@ router.get('/allprocesses', async function(req, res) {
     res.send(student)
 })
 
-router.post('/studentPage/addInterview/:studentName/:proccessId', async function(req, res) {
+router.post('/addInterview/:studentName/:proccessId', async function(req, res) {
     const interview = req.body
     let newInterview = new Interview({
         type: interview.type,
         date: interview.date,
         description: interview.description
     })
-    newProcess.save()
-    let processes = await Student.find({ name: req.params.studentName }, { $push: { Processes: newProcess } }, { Proccesses: 1 }, { new: true });
-    console.log(processes)
-    res.send(processes)
+    newInterview.save()
+    let student = await Student.find({ name: req.params.studentName })
+    console.log(student)
+    let process = await Process.findOneAndUpdate({ StudentId: student[0]._id, Id: req.params.proccessId }, { $push: { Interviews: newInterview } }, { new: true })
+        .populate("Interviews")
+        .exec(async function(err, student) {
+            console.log(student);
+            res.send(student)
+        })
+
 
 })
 module.exports = router;
