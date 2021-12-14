72be9a85559a459edaacccd0ff440ec4d6f8b242
osamabayatra <osama_bayatra@hotmail.com>
2021-12-14 20:54:18 +0200

    commit changes

--- server/routes/studentApi.js
+++ server/routes/studentApi.js
@@ -4,33 +4,7 @@ const Student = require('../models/Student')
 const Process = require('../models/Process')
 const Interview = require('../models/Interview')
 
-<<<<<<< HEAD
-// router.post('/process/:studentName', async function (req, res) {
-//     let process = req.body
-//     const student = await Student.find({ name: req.params.studentName })
-//     let newProcess = new Process({
-//         Id: student.counter++,
-//         JobTitle: process.JobTitle,
-//         companyName: process.companyName,
-//         Status: process.Status,
-//         link: process.link
-//     })
-//     process1.save()
-//     student = await Student.findOneAndUpdate({ name: req.params.studentName }, { $push: { Processes: process1 } }, { new: true });
-//     res.send(student)
-// })
-
-
-
 router.post('/process/:studentName', async function (req, res) {
-
-    res.send(student)
-})
-
-
-router.get('/allprocesses/:studentName', async function (req, res) {
-=======
-router.post('/process/:studentName', async function(req, res) {
     let process = req.body
     let student = await Student.find({ name: req.params.studentName })
     console.log(student)
@@ -50,18 +24,17 @@ router.post('/process/:studentName', async function(req, res) {
     res.send(student)
 })
 
-router.get('/allprocesses/:studentName', async function(req, res) {
->>>>>>> main
+router.get('/allprocesses/:studentName', async function (req, res) {
     const student = await Student.find({ name: req.params.studentName })
     res.send(student)
 })
 
-router.get('/allprocesses', async function(req, res) {
+router.get('/allprocesses', async function (req, res) {
     const student = await Process.find({})
     res.send(student)
 })
 
-router.post('/studentPage/addInterview/:studentName/:proccessId', async function(req, res) {
+router.post('/studentPage/addInterview/:studentName/:proccessId', async function (req, res) {
     const interview = req.body
     let newInterview = new Interview({
         type: interview.type,
