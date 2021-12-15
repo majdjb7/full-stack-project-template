03d22e2ae48c5d41ff3124915b5c929e8fc0388a
osamabayatra <osama_bayatra@hotmail.com>
2021-12-15 21:24:04 +0200

    ljdslj

--- dist/loginPage.js
+++ dist/loginPage.js
@@ -1,33 +1,32 @@
-$('.btn-login').on('click', async function() {
+$('.btn-login').on('click', function () {
     let username = $('#login-email').val()
     let password = $('#login-password').val()
 
-    const login = function(username, password) {
-        $.ajax({
-            method: "GET",
-            url: `/loginpage/login/${username}/${password}`,
-            success: user => {
-                if (user) {
-                    sessionStorage.setItem('user', JSON.stringify(user));
-                    user.Admin ? window.location.replace("/admin/adminPage.html") : window.location.replace("/student/studentPage.html")
-                } else {
-                    alert("user not found")
-                    location.reload()
-                }
-            },
-            error: function(xhr, text, err) {
-                alert("wrong password")
+    $.ajax({
+        method: "GET",
+        url: `/loginpage/login/${username}/${password}`,
+        success: function (user) {
+            if (user) {
+                sessionStorage.setItem('user', JSON.stringify(user));
+                user.Admin ? window.location.replace("/admin/adminPage.html") : window.location.replace("/student/studentPage.html")
+            } else {
+                alert("user not found")
+                sessionStorage.setItem('user', JSON.stringify(''));
+
                 location.reload()
             }
-        })
-    }
-    login(username, password)
+        },
+        error: function (xhr, text, err) {
+            alert("wrong password")
+            location.reload()
+        }
+    })
 })
 
 const switchers = [...document.querySelectorAll('.switcher')]
 
 switchers.forEach(item => {
-    item.addEventListener('click', function() {
+    item.addEventListener('click', function () {
         switchers.forEach(item => item.parentElement.classList.remove('is-active'))
         this.parentElement.classList.add('is-active')
     })
